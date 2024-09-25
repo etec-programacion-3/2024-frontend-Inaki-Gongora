18bb65a7c156fb223811ffba305b2eea3545472f
tomielprodelsoftware <t.fuentes@alumno.etec.um.edu.ar>
2024-09-25 12:06:48 -0300

    25-09-24 Login

--- src/pages/Usuario/Login.jsx
+++ src/pages/Usuario/Login.jsx
@@ -1,8 +1,47 @@
-// src/pages/Login/Login.jsx
-import React from 'react';
+import React, { useState } from 'react';
+import './Login.css';
 
 const Login = () => {
-  return <div>Login Page</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí podrías agregar la lógica para enviar el formulario
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <h2>Login Page</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              className='email'
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="login-button">Login</button>
+          <button type="button" className="create-button">Crear cuenta</button> {/* Cambiado a type="button" */}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
