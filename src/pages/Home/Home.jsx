0515158eef3ee22f29dd3073a58a7fe7a6bbe96d
Inaki-Gongora <i.gongora@alumno.etec.um.edu.ar>
2024-10-16 11:56:49 -0300

    16-10-24 Estilos login

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,10 @@
 // src/pages/Home.js
 import React from 'react';
-// import MenuDesplegable from '../components/MenuDesplegable.js';
-import './Home.css';
+// import './Home.css';
 const Home = () => {
   return (
-    <div className="fondo">
-      {/* <MenuDesplegable /> */}
-      <main>
-        <h1 className="titulo-landing-main">ZEPHYR<span className="marca-registrada">&reg;</span></h1>
-        <div className="nueva-coleccion">
-        
-          <p className="nueva-coleccion-texto">NUEVA COLECCIÓN - 2024</p>
-        </div>
-        <div className="nuestros-productos">
-          <h1 className="nuestros-productos-texto">EXPLORAR COLECCIONES</h1>
-        </div>
-        <section className="seccion-ver-productos">
-          <div className="ver-anillos">
-            <a href="/anillos" className="ver-productos">ANILLOS</a>
-          </div>
-          <div className="ver-aros">
-            <a href="/aros" className="ver-productos">AROS</a>
-          </div>
-        </section>
-      </main>
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi libero mollitia earum velit quam numquam nihil incidunt quia officiis necessitatibus? Magnam nemo molestias quo, distinctio veniam nobis non praesentium veritatis?</p>
     </div>
   );
 };
