bb8f5160819f2ff9942a9e25d2fccf9d8df98294
tomielprodelsoftware <t.fuentes@alumno.etec.um.edu.ar>
2024-10-25 12:27:03 -0300

    25-10-24-Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
 // src/pages/Home.js
 import React from 'react';
-// import './Home.css';
+import './Home.css';
+
 const Home = () => {
   return (
-    <div>
+  <div>
+    <div className='Imagen-fondo'>
+      <p>Zephyr</p>
     </div>
+
+    <section className='seccion'>
+      <div className='div-imagenes'>
+        <h1> Anillos</h1>
+      </div>
+      <div className= "div-imagenes1">
+        <h1> Aritos </h1>
+      </div>
+    </section>
+  </div>
   );
 };
 
