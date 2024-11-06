cefd771222bc5a6429b493470d52a02ff5f186ff
Inaki-Gongora <i.gongora@alumno.etec.um.edu.ar>
2024-11-06 09:03:14 -0300

    06-11-24 ProductoDetalle crudo

--- src/pages/ProductoDetalle/ProductoDetalle.jsx
+++ src/pages/ProductoDetalle/ProductoDetalle.jsx
@@ -5,13 +5,13 @@ import { fetchProductoById } from '../../services/api';
 import './ProductoDetalle.css';
 
 const ProductoDetalle = () => {
-  const { id } = useParams(); // Cambiado a 'id'
+  const { id } = useParams();
   const [producto, setProducto] = useState(null);
 
   useEffect(() => {
     const getProducto = async () => {
       try {
-        const data = await fetchProductoById(id); // Usando el id del producto
+        const data = await fetchProductoById(id);
         setProducto(data);
       } catch (error) {
         console.error('Error loading producto:', error);
@@ -24,11 +24,44 @@ const ProductoDetalle = () => {
   if (!producto) return <p>Loading...</p>;
 
   return (
-    <div className="producto-detalle-container">
-      <h1>{producto.nombre}</h1>
-      <p><strong>Descripción:</strong> {producto.descripcion}</p>
-      <p><strong>Precio:</strong> ${producto.precio}</p>
-      {/* Agrega más detalles si están disponibles, como categoría, stock, etc. */}
+    <div className="cuerpo">
+      <div className="contenedor-grande">
+        <div className="foto-producto">
+          <div className="contenedor">
+            <img className="imagen-producto" src={producto.imagenUrl} alt={producto.nombre} />
+          </div>
+        </div>
+
+        <div className="info-producto">
+          <h1>{producto.nombre}</h1>
+          <h2>${producto.precio}</h2>
+          <h4 className="pago">{producto.disponible ? 'Disponible' : 'No disponible'}</h4>
+          <h3>Género: {producto.genero}</h3>
+
+          <h4>Talle:</h4>
+          <div className="talles">
+            {producto.talles?.map((talle, index) => (
+              <div key={index} className="div-talles">
+                <h3>{talle}</h3>
+              </div>
+            ))}
+          </div>
+
+          <h4>Color:</h4>
+          <div className="talles">
+            <h3>{producto.color}</h3>
+          </div>
+
+          <h4 className="guia">Guía de talles</h4>
+
+          <div className="compra-container">
+            <button className="compra">Comprar</button>
+          </div>
+
+          <h1 className="zapas-descrip-titulo">Descripción</h1>
+          <h3 className="info-zapatillas">{producto.descripcion}</h3>
+        </div>
+      </div>
     </div>
   );
 };
