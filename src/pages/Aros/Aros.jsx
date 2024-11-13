bfb3b4cafb1cf61073999268ae143970615f1745
Inaki-Gongora <i.gongora@alumno.etec.um.edu.ar>
2024-11-13 10:25:42 -0300

    13-11-24 Productos funcional

--- src/pages/Aros/Aros.jsx
+++ src/pages/Aros/Aros.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'; // Importamos Link para redirigir
 import './Aros.css';
-import { AiOutlineHeart } from 'react-icons/ai';
 import fotito from "../../assets/foto-producto2.png"; // Imagen predeterminada en caso de no tener imagen
 
 import { fetchProductos } from '../../services/api'; // Importa la función para obtener productos
@@ -35,7 +35,7 @@ const Aros = () => {
 
   return (
     <div className="anillos-container">
-      <header className="aros-header">
+      <header className="anillos-header">
         <h1 className="anillos-title">Aros colgantes</h1>
         <h3 className="anillos-subtitle">
           Los aros colgantes de Zephyr son elementos elegantes y sofisticados, con cristales que llevan todo el encanto y la magia de la marca a los cuatro rincones del mundo.
@@ -43,24 +43,22 @@ const Aros = () => {
       </header>
       <div className="anillos-grid">
         {productos.map((producto) => (
-          <div className="producto-card" key={producto.id}>
+          <Link to={`/producto/${producto.id}`} className="producto-card" key={producto.id}> {/* Link que redirige al detalle del producto */}
             <img src={producto.imagen || fotito} alt={producto.nombre} className="imagenloca" />
             <h2 className="producto-nombre">{producto.nombre}</h2>
-            <p className="producto-precio-descuento">
+            <p className="producto-precio">
               {
-                // Verifica si el precio es un número válido antes de aplicar toFixed
-                typeof producto.precio === "number" && !isNaN(producto.precio) 
-                ? `$${producto.precio.toFixed(2)}`
-                : "Precio no disponible"
+                // Aseguramos que el precio es un número y es válido
+                !isNaN(producto.precio) && producto.precio !== null
+                  ? `$${parseFloat(producto.precio).toFixed(2)}`
+                  : "Precio no disponible"
               }
             </p>
-            <p className="producto-cuotas">Cuotas: {producto.cuotas}</p>
-            <AiOutlineHeart className="icono-favorito" />
-          </div>
+          </Link>
         ))}
       </div>
     </div>
   );
 };
 
-export default Aros
+export default Aros;
